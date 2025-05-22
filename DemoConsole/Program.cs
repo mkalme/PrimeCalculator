a24c8323486b88267bace87bcbda878fe6b1d526
mkalme <mikelis.kalme@gmail.com>
2025-05-22 18:36:47 +0300

    Add project files.

---
+++ DemoConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DemoConsole {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            PrimeCalculator.PrimeCalculator.CalculatePrimes(1000000);
+
+            watch.Stop();
+            Console.WriteLine($"Elapsed milliseconds: {watch.ElapsedMilliseconds}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PrimeCalculator/PrimeCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PrimeCalculator {
+    public static class PrimeCalculator {
+        public static ICollection<long> CalculatePrimes(int count) {
+            ConcurrentDictionary<long, byte> primes = new ConcurrentDictionary<long, byte>();
+
+            Parallel.For(2, count, i => {
+                if (IsPrime(i)) primes.TryAdd(i, 0);
+            });
+
+            return primes.Keys;
+        }
+
+        public static bool IsPrime(long n) {
+            long max = n / 2;
+
+            for (long i = 2; i <= max; i++) {
+                if (n % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
